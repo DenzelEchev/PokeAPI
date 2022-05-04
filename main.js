0768da072f3214588895ce6e3378cc3fb0455794
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2022-05-04 03:57:31 -0700

    the JS killed me a bit

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-fetch()
+const apiData = {
+    URL: 'https://pokeapi.co/api/v2/',
+    type: 'pokemon',
+    id: '22',
+}
+
+const {URL, type, id} = apiData
+const apiUrl = `${URL}${type}/${id}`
+
+fetch(apiUrl)
+    .then((data) => data.json())
+    .then((pokemon) => generatePokemon(pokemon))
+
+    const generatePokemon = (data) => {
+        console.log(data)
+        const html = `
+        <div class="Pokemon">${data.name}</div>
+        <img src=${data.sprites.front_shiny}>
+        <div class="details">
+        <span>Height:${data.height}</span>
+        <span>Weight:${data.weight}</span>
+        </div>
+        `
+
+        const pokemonDiv = document.querySelector('.pokemon')
+        pokemonDiv.innerHTML = html
+    }
+
+ 
