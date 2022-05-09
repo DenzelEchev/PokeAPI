3f8839572b04fb21f67bff902f600a1fdbcc713e
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2022-05-09 16:06:06 -0700

    added user input feature in JS

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-const apiData = {
-    URL: 'https://pokeapi.co/api/v2/',
-    type: 'pokemon',
-    id: '245', //make it so that you can type in a number and get back the data that way
+//get the value from the input, pass it into fetch
+const endpoint = 'https://pokeapi.co/api/v2/pokemon'
+document.querySelector('#button').addEventListener('click', fetchPokemon)
+
+function fetchPokemon(pokemonId){
+    pokemonId = document.querySelector('#pokeNum').value
+    fetch(`${endpoint}/${pokemonId}`)
+    .then((data) => data.json())
+    .then((pokeData) => (generatePokemon(pokeData)))
+
 }
 
-const {URL, type, id} = apiData
-const apiUrl = `${URL}${type}/${id}`
 
-fetch(apiUrl)
-    .then((data) => data.json())
-    .then((pokemon) => generatePokemon(pokemon))
+const generatePokemon = (data) => {
+    const html = `
+    <div class="Pokemon">${data.name}</div>
+    <img src=${data.sprites.front_default}>
+    <div class="details">
+    <span>Height:${data.height}</span>
+    <span>Weight:${data.weight}</span>
+    </div>
+    `
 
-    const generatePokemon = (data) => {
-        console.log(data)
-        const html = `
-        <div class="Pokemon">${data.name}</div>
-        <img src=${data.sprites.front_default}>
-        <div class="details">
-        <span>Height:${data.height}</span>
-        <span>Weight:${data.weight}</span>
-        </div>
-        `
+    const pokemonDiv = document.querySelector('.pokemon')
+    pokemonDiv.innerHTML = html
+}
 
-        const pokemonDiv = document.querySelector('.pokemon')
-        pokemonDiv.innerHTML = html
-    }
-
- 
