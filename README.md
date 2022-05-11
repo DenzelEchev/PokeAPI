ce333f0e1ae36fcd2c97ca4425d6000d30be80a6
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2022-05-11 10:15:21 -0700

    alot added in js and new css file

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ Pokemon API project
 This repo serves as a practice ground for my APIs and other JS techniques that I will need in the very near future.
 
 Things to do:
--Clean up html and form a structure
--add css and make it look more pretty
+[] Clean up html and form a structure
+[] add css and make it look more pretty(maybe have a static pokeDex image and put Data on top of it or make a dex looking object using css)
 
 JS Things for functionality:
--Add ID search function(done and bug checked)
--Add function for name as well in case of name given instead of number(WIP)
--When done with above feature add an auto complete function
--
+[x] Add ID search function
+[] Add functionality for name as well in case of name given instead of number
+[] Add a button that will switch you to the next pokemon from the one you are on.
+[] When done with above feature add an auto complete function
+[x] Make Background turn color to match the type of current Pokemon
+
+Things Fixed:
+[x] Replaced GBA game sprites with the official artwork from the game's illustrators
+[x] Right now on getting single type pokemon type info works, so it needs to work if it has either 1 or 2 types
--- main.js
+++ main.js
@@ -8,25 +8,43 @@ function fetchPokemon(pokemonId){
     
     if(pokemonId == parseInt(pokemonId) && parseInt(pokemonId) < 898 && parseInt(pokemonId) !== 0){
     fetch(`${endpoint}/${pokemonId}`)
-    .then((data) => data.json())
+    .then((response) => response.json())
     .then((pokeData) => (generatePokemon(pokeData)))
     } else {
         console.log('error')
     }
     } 
 
+    function findTypeForClass(pokemon){
+        if(pokemon.types.length > 0){
+            return pokemon.types[0].type.name
+        }else{
+            return ""
+        }
+    }
 
 const generatePokemon = (data) => {
     const html = `
-    <div class="Pokemon">${data.name}</div>
-    <img src=${data.sprites.front_default}>
-    <div class="details">
-    <span>Height:${data.height/10}m</span>
-    <span>Weight:${data.weight/10}kg</span>
+    <div class="pokeFetch ${findTypeForClass(data)}">
+        <div class="imgcard ${findTypeForClass(data)}">
+        <img src="${data.sprites.other["official-artwork"].front_default}">
+        </div>
+        <section class="details">
+        <div class="pokeName ${findTypeForClass(data)}">#${data.id}&nbsp| ${data.name[0].toUpperCase() + data.name.substring(1)}</div>
+        <div class="stats ${findTypeForClass(data)}">
+        <span>Height:${data.height/10}m &nbsp| Weight:${data.weight/10}kg</span>
+        </div>
+        ${data.types.map(type => `
+            <span class="${type.type.name}">${type.type.name}</span>
+        `)}
+        </section>
     </div>
     `
-
-    const pokemonDiv = document.querySelector('.pokemon')
+    const pokemonDiv = document.querySelector('.pokeContainer')
     pokemonDiv.innerHTML = html
+
+
 }
+
+
 
