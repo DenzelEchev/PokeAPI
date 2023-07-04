475fbfcc1945ccc663a6b8bb25b7653017d826df
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-07-04 02:59:00 -0400

    project start and vite install

---
+++ vite-pokeapi/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <h1 className="text-3xl font-bold underline text-teal-600">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
